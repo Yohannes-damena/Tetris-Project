64dfa0f4d9741a7e67618129f1bb9335477c7bee
Yohannes <yohannes.ugr-3245-16@aau.edu.et>
2026-01-31 08:54:51 +0300

    Made changes to gameOver Function:

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,26 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
-
-
 using namespace std;
+
 // Tetrominoes shapes
-enum Block{BarBlock,BoxBlock,TBlock,LBlock,JBlock,ZBlock,SBlock};
-enum Orientation {UP,RIGHT,DOWN, LEFT};
+enum Block
+{
+    BarBlock,
+    BoxBlock,
+    TBlock,
+    LBlock,
+    JBlock,
+    ZBlock,
+    SBlock
+};
+enum Orientation
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
 
 // Tetrominos and colors (7 blocks)
 const array<string, 7> tetromino = {
@@ -22,7 +36,8 @@ const array<string, 7> tetromino = {
     "..X..XX..X......"  // SBlock
 };
 
-struct ActiveBlock{
+struct ActiveBlock
+{
     Block block;
     Orientation orientation;
     int x;
@@ -42,8 +57,9 @@ const Color cyan = {21, 204, 209, 255};
 const Color blue = {13, 64, 216, 255};
 const Color lightBlue = {59, 85, 162, 255};
 const Color darkBlue = {44, 44, 127, 255};
-const Color GRID_LINE_COLOR = blue;
-const Color PIECE_COLORS[7] = { purple, yellow, green, red, orange,cyan, blue };
+// const Color blue = blue;
+const Color PIECE_COLORS[7] = {purple, yellow, green, red, orange, cyan, blue};
+
 // Board size
 const int BOARD_WIDTH = 300;
 const int BOARD_HEIGHT = 600;
@@ -60,10 +76,10 @@ const int BOARD_Y = 0;
 // Scoreboard (UI panel position & size)
 const int SCORE_X = BOARD_WIDTH;
 const int SCORE_Y = 0;
-const int SCORE_WIDTH  = INF_AREA;
+const int SCORE_WIDTH = INF_AREA;
 const int SCORE_HEIGHT = BOARD_HEIGHT;
 
-int field[COL_NB * ROW_NB];
+int field[COL_NB * ROW_NB]; //[20 * 30]
 bool hasActiveBlock = false;
 bool gameEnd = false;
 int score = 0;
@@ -72,42 +88,48 @@ int score = 0;
 
 void drawGrid();
 void drawBackground();
-void scoreBoard();
 void playGame(ActiveBlock &activeBlock);
-void spawnBlock(ActiveBlock &activeBlock);
 void drawActiveBlock(const ActiveBlock &activeBlock);
+void spawnBlock(ActiveBlock &activeBlock);
 bool blockCanGoDown(const ActiveBlock &activeBlock);
 void lockActiveBlock(const ActiveBlock &activeBlock);
-void drawLockedCells();
-void gameOver();
 int Rotate(int x, int y, int r);
 void movePiece(ActiveBlock &activeBlock);
 bool DoesPieceFit(const ActiveBlock &activeBlock);
+void drawLockedCells();
+void gameOver();
 int handleLineClearsAndScore();
+void scoreBoard();
 
 // Game Window Setup
-void init() {
-    InitWindow(BOARD_WIDTH + INF_AREA, BOARD_HEIGHT , "TETRIS GAME");
+void init()
+{
+    InitWindow(BOARD_WIDTH + INF_AREA, BOARD_HEIGHT, "TETRIS GAME");
     SetTargetFPS(60);
     // Clear the field to ensure no garbage blocks (-1 means empty)
     fill_n(field, COL_NB * ROW_NB, -1);
 }
 
-int main() {
+int main()
+{
     init();
-    ActiveBlock activeBlock;
-    while (!WindowShouldClose()){
+    ActiveBlock activeBlock; // Init out struct Activeblock
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
         ClearBackground(darkBlue);
         drawBackground();
         drawGrid();
-        if (!gameEnd){
+        if (!gameEnd)
+        {
             movePiece(activeBlock);
             playGame(activeBlock);
             DoesPieceFit(activeBlock);
             handleLineClearsAndScore();
             scoreBoard();
-        } else {
+        }
+        else
+        {
             // Still draw locked cells and show Game Over message
             drawLockedCells();
             gameOver();
@@ -126,44 +148,51 @@ void drawBackground()
         BOARD_Y,
         BOARD_WIDTH,
         BOARD_HEIGHT,
-        darkGrey
-    );
+        darkGrey);
     // Scoreboard background
     DrawRectangle(
         SCORE_X,
         SCORE_Y,
         SCORE_WIDTH,
         SCORE_HEIGHT,
-        darkBlue
-    );
+        darkBlue);
 }
 
-void drawGrid() {
+void drawGrid()
+{
     int startPosX = 0;
     int startPosY = 0;
-    for(int row = 0; row <= ROW_NB; row++){
-        DrawLine(startPosX,row*CELL_HEIGHT,BOARD_WIDTH, row*CELL_HEIGHT,GRID_LINE_COLOR);
+    for (int row = 0; row <= ROW_NB; row++)
+    { // y coordinate
+        DrawLine(startPosX, row * CELL_HEIGHT, BOARD_WIDTH, row * CELL_HEIGHT, blue);
     }
-    for(int col = 0; col <= COL_NB; col++){
-        DrawLine(col *CELL_WIDTH,startPosY,col*CELL_WIDTH, BOARD_HEIGHT, GRID_LINE_COLOR);
+    for (int col = 0; col <= COL_NB; col++)
+    { // x coordinate
+        DrawLine(col * CELL_WIDTH, startPosY, col * CELL_WIDTH, BOARD_HEIGHT, blue);
     }
 }
 
-//function for the spawn blocks
-void spawnBlock(ActiveBlock &activeBlock){
-    activeBlock.block = Block(GetRandomValue(BarBlock,SBlock));
-    activeBlock.orientation = Orientation(GetRandomValue(UP,LEFT));
+// function for the spawn blocks
+void spawnBlock(ActiveBlock &activeBlock)
+{
+    activeBlock.block = Block(GetRandomValue(BarBlock, SBlock));
+    activeBlock.orientation = Orientation(GetRandomValue(UP, LEFT));
     activeBlock.x = COL_NB / 2 - 2;
     activeBlock.y = 0;
-        // If the spawned piece immediately overlaps locked cells, set game over
-    for (int px = 0; px < 4; px++) {
-        for (int py = 0; py < 4; py++) {
+    // If the spawned piece immediately overlaps locked cells, set game over
+    for (int px = 0; px < 4; px++)
+    {
+        for (int py = 0; py < 4; py++)
+        {
             int idx = Rotate(px, py, activeBlock.orientation);
-            if (tetromino[activeBlock.block][idx] == 'X') {
+            if (tetromino[activeBlock.block][idx] == 'X')
+            {
                 int fx = activeBlock.x + px;
                 int fy = activeBlock.y + py;
-                if (fx >= 0 && fx < COL_NB && fy >= 0 && fy < ROW_NB) {
-                    if (field[fy * COL_NB + fx] != -1) {
+                if (fx >= 0 && fx < COL_NB && fy >= 0 && fy < ROW_NB)
+                {
+                    if (field[fy * COL_NB + fx] != -1)
+                    {
                         gameEnd = true;
                         return;
                     }
@@ -175,25 +204,27 @@ void spawnBlock(ActiveBlock &activeBlock){
     activeBlock.colorIndex = static_cast<int>(activeBlock.block);
     activeBlock.color = PIECE_COLORS[activeBlock.colorIndex];
 }
-void drawActiveBlock(const ActiveBlock &activeBlock){
-    for(int x = 0; x < 4; x++){
-        for(int y = 0; y < 4; y++){
-            int index = Rotate(x, y , activeBlock.orientation);
-            if(tetromino[activeBlock.block][index] == 'X'){
+void drawActiveBlock(const ActiveBlock &activeBlock)
+{
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
+            int index = Rotate(x, y, activeBlock.orientation);
+            if (tetromino[activeBlock.block][index] == 'X')
+            {
                 DrawRectangle(
                     (activeBlock.x + x) * CELL_WIDTH,
                     (activeBlock.y + y) * CELL_HEIGHT,
                     CELL_WIDTH,
                     CELL_HEIGHT,
-                    activeBlock.color
-                );
+                    activeBlock.color);
                 DrawRectangleLines(
                     (activeBlock.x + x) * CELL_WIDTH,
                     (activeBlock.y + y) * CELL_HEIGHT,
                     CELL_WIDTH,
                     CELL_HEIGHT,
-                    WHITE
-                );
+                    darkBlue);
             }
         }
     }
@@ -229,36 +260,97 @@ void playGame(ActiveBlock &activeBlock)
     drawLockedCells();
     drawActiveBlock(activeBlock);
 }
-void gameOver(){
-     const char *msg = "GAME OVER";
-            int fontSize = 28;
-            int textW = MeasureText(msg, fontSize);
-            DrawText(msg, BOARD_WIDTH + (INF_AREA/2) - textW/2, BOARD_HEIGHT/2 - fontSize, fontSize, RAYWHITE);
-            const char *msg2 = "Press R to restart";
-            int fs2 = 14;
-            DrawText(msg2, BOARD_WIDTH + (INF_AREA/2) - MeasureText(msg2, fs2)/2, BOARD_HEIGHT/2 + 10, fs2, RAYWHITE);
+void gameOver()
+{
+//     const char *title = "GAME OVER";
+//     const char *scoreLabel = "FINAL SCORE";
 
-            // Restart
-            if(IsKeyPressed(KEY_R)){
-                fill_n(field, COL_NB * ROW_NB, -1);
-                score = 0;
-                hasActiveBlock = false;
-                gameEnd = false;
-            }
+//     int titleFontSize = 50;
+//     int scoreFontSize = 18;
+
+//     int textW = MeasureText(title, titleFontSize);
+//     DrawText(title, BOARD_WIDTH + (INF_AREA / 2) - textW / 2, BOARD_HEIGHT / 2 - titleFontSize, titleFontSize, RAYWHITE);
+
+//     DrawText(
+//         TextFormat("%d", score),
+//         (BOARD_WIDTH / 2) - MeasureText(TextFormat("%d", score), scoreFontSize) / 2,
+//         BOARD_HEIGHT / 2 + titleFontSize,
+//         scoreFontSize,
+//         BLACK);
+//     // DrawText(msg3, BOARD_WIDTH + (INF_AREA/2) - MeasureText(msg3, fs2)/2, BOARD_HEIGHT/2 + 20, fs2, RAYWHITE);
+
+//     const char *msg2 = "Press R to restart";
+//     DrawText(msg2, BOARD_WIDTH + (INF_AREA / 2) - MeasureText(msg2, scoreFontSize) / 2, BOARD_HEIGHT / 2 + 10, scoreFontSize, RAYWHITE);
+//     // Restart
+
+ const char* title = "GAME OVER";
+    const char* scoreLabel = "FINAL SCORE";
+
+    int titleFontSize = 32;
+    int scoreFontSize = 16;
+
+    int titleWidth = MeasureText(title, titleFontSize);
+    int scoreWidth = MeasureText(scoreLabel, scoreFontSize);
+
+    int centerX = BOARD_WIDTH +( INF_AREA /2);
+    int spaceY = BOARD_WIDTH /2;
+    DrawText(
+        title,
+        centerX - titleWidth / 2,
+        spaceY,
+        titleFontSize,
+        RED
+    );
+
+    DrawText(
+        scoreLabel,
+        centerX - scoreWidth / 2,
+        spaceY + 50 ,
+        scoreFontSize,
+        DARKGRAY
+    );
+
+    DrawText(
+        TextFormat("%d", score),
+        centerX - MeasureText(TextFormat("%d", score), scoreFontSize) / 2,
+        spaceY + 100,
+        scoreFontSize,
+        BLACK
+    );
+    DrawText(
+        "Press R to restart",
+        centerX - MeasureText("Press R to restart", 20) / 2,
+        spaceY + 150,
+        20,
+        DARKBLUE
+    );
+    if (IsKeyPressed(KEY_R))
+    {
+        fill_n(field, COL_NB * ROW_NB, -1);
+        score = 0;
+        hasActiveBlock = false;
+        gameEnd = false;
+    }
 }
-// Lock active block into the field (simple implementation)
-void lockActiveBlock(const ActiveBlock &activeBlock){
-    for(int x = 0; x < 4; x++){
-        for(int y = 0; y < 4; y++){
+// Lock active block into the field
+void lockActiveBlock(const ActiveBlock &activeBlock)
+{
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
             int index = Rotate(x, y, activeBlock.orientation);
-            if(tetromino[activeBlock.block][index] == 'X'){
+            if (tetromino[activeBlock.block][index] == 'X')
+            {
                 int fx = activeBlock.x + x;
                 int fy = activeBlock.y + y;
-                if(fx >= 0 && fx < COL_NB && fy >= 0 && fy < ROW_NB){
+                if (fx >= 0 && fx < COL_NB && fy >= 0 && fy < ROW_NB)
+                {
                     field[fy * COL_NB + fx] = activeBlock.colorIndex;
 
                     // If any locked cell reaches the top row (y == 0), game over
-                    if(fy <= 0){
+                    if (fy <= 0)
+                    {
                         gameEnd = true;
                     }
                 }
@@ -266,7 +358,7 @@ void lockActiveBlock(const ActiveBlock &activeBlock){
         }
     }
 }
-bool DoesPieceFit( const ActiveBlock &activeBlock)
+bool DoesPieceFit(const ActiveBlock &activeBlock)
 {
     // Use the global field array
     for (int px = 0; px < 4; px++)
@@ -277,19 +369,21 @@ bool DoesPieceFit( const ActiveBlock &activeBlock)
             int fy = activeBlock.y + py;
             if (fx >= 0 && fx < COL_NB && fy >= 0 && fy < ROW_NB)
             {
-                if (tetromino[activeBlock.block][index] == 'X' &&  field[fy * COL_NB + fx] != -1)
+                if (tetromino[activeBlock.block][index] == 'X' && field[fy * COL_NB + fx] != -1)
                     return false;
             }
             else
             {
-                if (tetromino[activeBlock.block][index] == 'X') return false;
+                if (tetromino[activeBlock.block][index] == 'X')
+                    return false;
             }
         }
     return true;
 }
-void movePiece( ActiveBlock &activeBlock)
+void movePiece(ActiveBlock &activeBlock)
 {
-    if (gameEnd) return;
+    if (gameEnd)
+        return;
 
     ActiveBlock test = activeBlock; // copy for testing
 
@@ -327,18 +421,23 @@ void movePiece( ActiveBlock &activeBlock)
         {
             test = activeBlock;
             test.y++;
-            if (!DoesPieceFit(test)) break;
+            if (!DoesPieceFit(test))
+                break;
             activeBlock.y++;
         }
     }
 }
 
-//Draw locked cells
-void drawLockedCells(){
-    for(int r = 0; r < ROW_NB; r++){
-        for(int c = 0; c < COL_NB; c++){
+// Draw locked cells
+void drawLockedCells()
+{
+    for (int r = 0; r < ROW_NB; r++)
+    {
+        for (int c = 0; c < COL_NB; c++)
+        {
             int idx = field[r * COL_NB + c];
-            if(idx != -1){
+            if (idx != -1)
+            {
                 DrawRectangle(c * CELL_WIDTH, r * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, PIECE_COLORS[idx]);
                 DrawRectangleLines(c * CELL_WIDTH, r * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT, BLACK);
             }
@@ -346,20 +445,27 @@ void drawLockedCells(){
     }
 }
 
-bool blockCanGoDown(const ActiveBlock &activeBlock){
-    for(int x= 0; x < 4; x++){
-        for(int y = 0; y < 4; y++){
+bool blockCanGoDown(const ActiveBlock &activeBlock)
+{
+    for (int x = 0; x < 4; x++)
+    {
+        for (int y = 0; y < 4; y++)
+        {
             int index = Rotate(x, y, activeBlock.orientation);
-            if(tetromino[activeBlock.block][index] == 'X'){
+            if (tetromino[activeBlock.block][index] == 'X')
+            {
                 int newX = activeBlock.x + x;
                 int newY = activeBlock.y + y + 1;
-                if(newY >=ROW_NB){
+                if (newY >= ROW_NB)
+                {
                     return false;
                 }
-                if(newX < 0 || newX >= COL_NB) {
+                if (newX < 0 || newX >= COL_NB)
+                {
                     return false;
                 }
-                if(field[newY * COL_NB + newX] != -1){ // Collision with a locked block.
+                if (field[newY * COL_NB + newX] != -1)
+                { // Collision with a locked block.
                     return false;
                 }
             }
@@ -370,37 +476,47 @@ bool blockCanGoDown(const ActiveBlock &activeBlock){
 
 int Rotate(int x, int y, int r)
 {
-	switch (r % 4)
-	{
-		case 0: return y * 4 + x;
-		case 1: return 12 + y - (x * 4);
-		case 2: return 15 - (y * 4) - x;
-		case 3: return 3 - y + (x * 4);
-	}
-	return 0;
+    switch (r % 4)
+    {
+    case 0:
+        return y * 4 + x;
+    case 1:
+        return 12 + y - (x * 4);
+    case 2:
+        return 15 - (y * 4) - x;
+    case 3:
+        return 3 - y + (x * 4);
+    }
+    return 0;
 }
-
 
 // Scoreboard and block clearing logic
 
-int handleLineClearsAndScore() {
+int handleLineClearsAndScore()
+{
     int linesCleared = 0;
 
     // Scan from bottom to top
-    for (int row = ROW_NB - 1; row >= 0; row--) {
+    for (int row = ROW_NB - 1; row >= 0; row--)
+    {
         bool full = true;
 
-        for (int col = 0; col < COL_NB; col++) {
-            if (field[row * COL_NB + col] == -1) {
+        for (int col = 0; col < COL_NB; col++)
+        {
+            if (field[row * COL_NB + col] == -1)
+            {
                 full = false;
                 break;
             }
         }
 
-        if (full) {
+        if (full)
+        {
             // Move everything above down
-            for (int y = row; y > 0; y--) {
-                for (int col = 0; col < COL_NB; col++) {
+            for (int y = row; y > 0; y--)
+            {
+                for (int col = 0; col < COL_NB; col++)
+                {
                     field[y * COL_NB + col] =
                         field[(y - 1) * COL_NB + col];
                 }
@@ -416,24 +532,33 @@ int handleLineClearsAndScore() {
     }
 
     // Scoring
-    switch (linesCleared) {
-        case 1: score += 100; break;
-        case 2: score += 300; break;
-        case 3: score += 500; break;
-        case 4: score += 800; break;
-        default: break;
+    switch (linesCleared)
+    {
+    case 1:
+        score += 100;
+        break;
+    case 2:
+        score += 300;
+        break;
+    case 3:
+        score += 500;
+        break;
+    case 4:
+        score += 800;
+        break;
+    default:
+        break;
     }
 
     return linesCleared;
 }
-void scoreBoard() {
+void scoreBoard()
+{
     DrawText("SCORE", SCORE_X + 40, SCORE_Y + 80, 28, BLACK);
     DrawText(
         TextFormat("%d", score),
         SCORE_X + 40,
         SCORE_Y + 40,
         28,
-        BLACK
-    );
+        BLACK);
 }
-
